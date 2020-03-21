c44e336320410510e2c879b1447a89c51ac3e9da
Albert Suarez <alsumo95@gmail.com>
2020-03-21 21:09:08 +0100

    Tweaked phase 1 for getting the result

--- src/day_1.py
+++ src/day_1.py
@@ -7,14 +7,14 @@ def phase_1():
     with open(os.path.join(DATA_FOLDER, 'day_1_1.txt')) as input_file:
         content_file = str(input_file.read())
     sequence = [int(i) for i in content_file.split()]
-    maximum = current = 0
+    maximum = current = 1
     for idx in range(0, len(sequence)):
         if idx == 0 or (sequence[idx] > sequence[idx - 1] and sequence[idx] % 2 != sequence[idx - 1] % 2):
             current += 1
+            if current > maximum:
+                maximum = current
         else:
-            current = 0
-        if current > maximum:
-            maximum = current
+            current = 1
     print(f'Solution: {maximum}')
 
 
