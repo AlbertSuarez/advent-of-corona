67b2d5d211ad6c3b648eb5d7dca3c46dc01138d2
Albert Suarez <alsumo95@gmail.com>
2020-03-21 21:15:59 +0100

    Implemented phase 2 of day 1

--- src/day_1.py
+++ src/day_1.py
@@ -18,5 +18,21 @@ def phase_1():
     print(f'Solution: {maximum}')
 
 
+def phase_2():
+    with open(os.path.join(DATA_FOLDER, 'day_1_1.txt')) as input_file:
+        content_file = str(input_file.read())
+    sequence = [int(i) for i in content_file.split()]
+    sequence.sort()
+    sequence = list(set(sequence))
+    maximum = current = 0
+    for idx in range(0, len(sequence)):
+        if idx == 0 or (sequence[idx] % 2 != sequence[idx - 1] % 2):
+            current += 1
+            if current > maximum:
+                maximum = current
+    print(f'Solution: {maximum}')
+
+
 if __name__ == '__main__':
     phase_1()
+    phase_2()
